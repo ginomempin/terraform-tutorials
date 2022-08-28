d2ef51412f93f081691b5c61e0cfeb6ff83004bc
gino <gino.mempin@gmail.com>
2022-08-28 14:31:04 +0900

    Finish "Output Data with Terraform"
    Reference:
    https://learn.hashicorp.com/tutorials/terraform/outputs

--- learn-terraform-docker-container/outputs.tf
+++ learn-terraform-docker-container/outputs.tf
@@ -1,9 +1,25 @@
-output "container_id" {
-  description = "ID of the Docker container"
-  value       = docker_container.nginx.id
-}
-
 output "image_id" {
   description = "ID of the Docker image"
   value       = docker_image.nginx.id
+}
+
+output "container_id_and_name" {
+  description = "ID of the Docker container"
+  value       = "${docker_container.nginx.id}:${docker_container.nginx.name}"
+}
+
+output "container_image" {
+  description = "IP of the Docker container"
+  value       = docker_container.nginx.image
+}
+
+output "container_user" {
+  description = "USER:GROUP used to run the container"
+  value       = docker_container.nginx.user
+  sensitive   = true
+}
+
+output "container_env_vars" {
+  description = "Environment Variables set on the Docker container"
+  value       = docker_container.nginx.env
 }
